6199fdc5fe2db8c7e0b405203a969fd4b6786a6e
kineva1992 <42785264+kineva1992@users.noreply.github.com>
2021-06-15 13:33:31 +0300

    Add folder route-planning
    Task PathFinderTask

---
+++ Leavel 1/route-planning/PathFinderTask.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace RoutePlanning
+{
+    public static class PathFinderTask
+    {
+        static double minLength = double.MaxValue;
+        public static int[] FindBestCheckpointsOrder(Point[] checkpoints)
+        {
+            var bestOrder = MakeTrivialPermutation(checkpoints, 
+                            new int[checkpoints.Length], new int[checkpoints.Length], 1, 0);
+            minLength = double.MaxValue;
+            return bestOrder;
+        }
+
+        private static int[] MakeTrivialPermutation(Point[] checkpoints, 
+                             int[] order, int[] bestOrder, int position, double path)
+        {
+            if (position == checkpoints.Length)
+            {
+                if (path < minLength)
+                    minLength = path;
+                return (int[])order.Clone();
+            }
+            for (int i = 1; i < order.Length; i++)
+            {
+                var index = Array.IndexOf(order, i, 0, position);
+                if (index != -1)
+                    continue;
+                order[position] = i;
+
+                var distance = PointExtensions.DistanceTo(checkpoints[order[position - 1]], 
+                checkpoints[order[position]]);
+                path += distance;
+                if (path > minLength)
+                    continue;
+                bestOrder = MakeTrivialPermutation(checkpoints, order, bestOrder, position + 1, path);
+                path -= distance;
+            }
+            return bestOrder;
+        }
+    }
+}
