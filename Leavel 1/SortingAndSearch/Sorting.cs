e612398850d4e15e122030cf4235b23d5287b7b6
alexandex <kienva1992@gmail.com>
2021-06-16 17:10:06 +0300

    feat: Add new function BubbleSortRange

--- Leavel 1/SortingAndSearch/Sorting.cs
+++ Leavel 1/SortingAndSearch/Sorting.cs
@@ -16,8 +16,18 @@ namespace SortingAndSearch
                         array[j + 1] = array[j];
                         array[j] = t;
                     }
+        }
 
-
+        public static void BubbleSortRange(int[] array, int left, int right)
+        {
+            for (int i = left; i < right; i++)
+                for (int j = left; j < right; j++)
+                    if (array[j] > array[j + 1])
+                    {
+                        var t = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = t;
+                    }
         }
 
     }
