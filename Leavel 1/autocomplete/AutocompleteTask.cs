a9a4465598ef916c3662ce8ba605865c17faa7d4
alexandex <kienva1992@gmail.com>
2021-06-17 21:42:11 +0300

    feat: add project Autocomplete

---
+++ Leavel 1/autocomplete/AutocompleteTask.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Autocomplete
+{
+    internal class AutocompleteTask
+    {
+        /// <returns>
+        /// Возвращает первую фразу словаря, начинающуюся с prefix.
+        /// </returns>
+        /// <remarks>
+        /// Эта функция уже реализована, она заработает, 
+        /// как только вы выполните задачу в файле LeftBorderTask
+        /// </remarks>
+        public static string FindFirstByPrefix(IReadOnlyList<string> phrases, string prefix)
+        {
+            var index = LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count) + 1;
+            if (index < phrases.Count && phrases[index].StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+                return phrases[index];
+            
+            return null;
+        }
+
+        /// <returns>
+        /// Возвращает первые в лексикографическом порядке count (или меньше, если их меньше count) 
+        /// элементов словаря, начинающихся с prefix.
+        /// </returns>
+        /// <remarks>Эта функция должна работать за O(log(n) + count)</remarks>
+        public static string[] GetTopByPrefix(IReadOnlyList<string> phrases, string prefix, int count)
+        {
+            var start = LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count) + 1;
+            var numbers = GetCountByPrefix(phrases, prefix);
+
+            if (numbers <= 0)
+            {
+                return new string[0];
+            }
+
+            if (count > numbers)
+            {
+                count = numbers;
+            }
+
+            var words = new string[count];
+
+            for (var i = 0; i < count; i++)
+            {
+                words[i] = phrases[start + i];
+            }
+
+            return words;
+        }
+
+        /// <returns>
+        /// Возвращает количество фраз, начинающихся с заданного префикса
+        /// </returns>
+        public static int GetCountByPrefix(IReadOnlyList<string> phrases, string prefix)
+        {
+            var left = LeftBorderTask.GetLeftBorderIndex(phrases, prefix, -1, phrases.Count);
+            var right = RightBorderTask.GetRightBorderIndex(phrases, prefix, -1, phrases.Count);
+            return right - left - 1;
+        }
+    }
+
+    [TestFixture]
+    public class AutocompleteTests
+    {
+        [Test]
+        public void TopByPrefix_IsEmpty_WhenNoPhrases()
+        {
+            // ...
+            //CollectionAssert.IsEmpty(actualTopWords);
+        }
+
+        // ...
+
+        [Test]
+        public void CountByPrefix_IsTotalCount_WhenEmptyPrefix()
+        {
+            // ...
+            //Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        // ...
+    }
+}
---
+++ Leavel 1/autocomplete/RightBorderTask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Autocomplete
+{
+    public class RightBorderTask
+    {
+        /// <returns>
+        /// Возвращает индекс правой границы. 
+        /// То есть индекс минимального элемента, который не начинается с prefix и большего prefix.
+        /// Если такого нет, то возвращает items.Length
+        /// </returns>
+        /// <remarks>
+        /// Функция должна быть НЕ рекурсивной
+        /// и работать за O(log(items.Length)*L), где L — ограничение сверху на длину фразы
+        /// </remarks>
+        public static int GetRightBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
+        {
+
+            while (right - left > 1)
+            {
+
+                var midle = (right - left) / 2 + left;
+
+                if (string.Compare(prefix, phrases[midle], StringComparison.OrdinalIgnoreCase) >= 0
+                    || phrases[midle].StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+                {
+                    left = midle;
+                }
+
+                else
+                {
+                    right = midle;
+                }
+
+            }
+
+            return right;
+        }
+    }
+}
