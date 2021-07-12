c148d55f821bf6054cd4b725f9fc7e139f8ea9c1
alexandex <kienva1992@gmail.com>
2021-07-12 18:02:45 +0300

    feat: add new class Book

---
+++ Leavel 1/BasicsOOP/questions/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicsOOP.questions
+{
+    class Book : IComparable
+    {
+        public string Title;
+        public int Theme;
+
+        public int CompareTo(object obj)
+        {
+            Book book = obj as Book;
+
+            if (Theme > book.Theme)
+            {
+                return 1;
+            }
+
+            else if (Theme == book.Theme)
+            {
+                return Title.CompareTo(book.Title);
+            }
+
+            else return -1;
+        }
+
+    }
+}
