c09606c7aac693e8d1498dcf51a4e8d21241bd35
alexandex <kienva1992@gmail.com>
2021-07-16 18:20:21 +0300

    feat: new project DataIntegrity for learn OOP.

---
+++ Leavel 1/DataIntegrity/Data/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataIntegrity.Data
+{
+    class Student
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value == null) throw new ArgumentException($"{Name} does not support null");
+                name = value;
+            }
+        }
+
+        private static void WriteStudent()
+        {
+            // ReadName считает неизвестно откуда имя очередного студента
+            var student = new Student { Name = ReadName() };
+            Console.WriteLine("student " + FormatStudent(student));
+        }
+
+        private static string FormatStudent(Student student)
+        {
+            return student.Name.ToUpper();
+        }
+
+        private static string ReadName()
+        {
+            return "John Daniel";
+        }
+    }
+}
---
+++ Leavel 1/DataIntegrity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataIntegrity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
