1c0b47a7dcc56bee6831fc6fd31bdef297f798c8
alexandex <kienva1992@gmail.com>
2021-07-18 16:51:37 +0300

    11

--- Leavel 1/DataIntegrity/Program.cs
+++ Leavel 1/DataIntegrity/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataIntegrity.Data;
+using System;
 
 namespace DataIntegrity
 {
@@ -6,7 +7,16 @@ namespace DataIntegrity
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ReadOnlyVector vector = new ReadOnlyVector(3, 4);
+
+            Console.WriteLine($"X = {vector.X}; Y = {vector.Y}");
+            Console.WriteLine($"Vector = {vector.Add(vector).X}; {vector.Add(vector).Y}");
+            Console.WriteLine(new string('-', 30));
+
+            ReadOnlyVector vector1 = vector.WithX(5);
+            Console.WriteLine($"X = {vector1.X}; Y = {vector1.Y}");
+            Console.WriteLine($"New vector = {vector.Add(vector1).X}; {vector.Add(vector1).Y}");
+            Console.WriteLine(new string('-', 30));
         }
     }
 }
