1bac2c9e6f7526b54d714362a0b8ad47bb5161b3
alexandex <kienva1992@gmail.com>
2021-07-18 14:12:19 +0300

    1

---
+++ Leavel 1/DataIntegrity/Data/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataIntegrity.Data
+{
+    class Book
+    {
+        public string Title { get; set; }
+
+        public static void Check()
+        {
+            var book = new Book();
+            book.Title = "Structure and interpretation of computer programs";
+            Console.WriteLine(book.Title);
+        }
+    }
+}
