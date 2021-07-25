4a67785e324ab18f0285583e715e6f2d4b7c8112
alexandex <kienva1992@gmail.com>
2021-07-25 11:54:59 +0300

    1

--- Leavel 1/StructBenchmarking/ExperimentsTask.cs
+++ Leavel 1/StructBenchmarking/ExperimentsTask.cs
@@ -7,33 +7,72 @@ namespace StructBenchmarking
         public static ChartData BuildChartDataForArrayCreation(
             IBenchmark benchmark, int repetitionsCount)
         {
-            var classesTimes = new List<ExperimentResult>();
-            var structuresTimes = new List<ExperimentResult>();
-            
-            //...
-
-            return new ChartData
-            {
-                Title = "Create array",
-                ClassPoints = classesTimes,
-                StructPoints = structuresTimes,
-            };
+            return BildingCharDataForClasses(benchmark, repetitionsCount, false, "Create array");
         }
 
         public static ChartData BuildChartDataForMethodCall(
             IBenchmark benchmark, int repetitionsCount)
         {
+            return BildingCharDataForClasses(benchmark, repetitionsCount, true, "Call method with argument");
+        }
+        public static ChartData BildingCharDataForClasses(IBenchmark benchmark, int repetitionsCount, bool isMethodCaller, string titleName)
+        {
             var classesTimes = new List<ExperimentResult>();
             var structuresTimes = new List<ExperimentResult>();
-            
-            //...
+
+            foreach (var size in Constants.FieldCounts)
+            {
+                var method = new CompariableMethods(size, isMethodCaller);
+                classesTimes.Add(new ExperimentResult(size, benchmark.MeasureDurationInMs(method.Classes, repetitionsCount)));
+                structuresTimes.Add(new ExperimentResult(size, benchmark.MeasureDurationInMs(method.Struktures, repetitionsCount)));
+            }
 
             return new ChartData
             {
-                Title = "Call method with argument",
+                Title = titleName,
                 ClassPoints = classesTimes,
-                StructPoints = structuresTimes,
+                StructPoints = structuresTimes
+
             };
+        }
+
+        //public static ChartData BuildChartDataForMethodCall(
+        //    IBenchmark benchmark, int repetitionsCount)
+        //{
+        //    var classesTimes = new List<ExperimentResult>();
+        //    var structuresTimes = new List<ExperimentResult>();
+            
+        //    //...
+
+        //    return new ChartData
+        //    {
+        //        Title = "Call method with argument",
+        //        ClassPoints = classesTimes,
+        //        StructPoints = structuresTimes,
+        //    };
+        //}
+
+        
+    }
+
+    public class CompariableMethods
+    { 
+    public ITask Classes { get; set; }
+    public ITask Struktures { get; set; }
+
+        public CompariableMethods(int size, bool isMethodCaller)
+        {
+            if (isMethodCaller)
+            {
+                Classes = new MethodCallWithClassArgumentTask(size);
+                Struktures = new MethodCallWithStructArgumentTask(size);
+            }
+
+            else
+            {
+                Classes = new ClassArrayCreationTask(size);
+                Struktures = new StructArrayCreationTask(size);
+            }
         }
     }
 }
