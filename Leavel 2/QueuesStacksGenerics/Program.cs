2c882f9267e828567c865b529e62bb1a752595b1
alexandex <kienva1992@gmail.com>
2021-07-25 17:27:38 +0300

    1

---
+++ Leavel 2/QueuesStacksGenerics/Program.cs
@@ -0,0 +1,25 @@
+﻿using QueuesStacksGenerics.Queues;
+using System;
+
+namespace QueuesStacksGenerics
+{
+  
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var queue = new Queue();
+            for (int i = 0; i < 3; i++)
+            {
+                queue.Enque(i);
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(queue.Dequeue());
+            }
+        }
+    }
+}
---
+++ Leavel 2/QueuesStacksGenerics/Queues/QueueItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueuesStacksGenerics.Queues
+{
+    public class QueueItem
+    {
+        public int Value { get; set; }
+        public QueueItem Next { get; set; }
+
+    }
+
+    public class Queue
+    {
+        QueueItem head;
+        QueueItem tail;
+
+        public void Enque(int value)
+        {
+            if (head == null)
+            {
+                tail = head = new QueueItem { Value = value, Next = null };
+            }
+
+            else
+            {
+                var item = new QueueItem { Value = value, Next = null };
+                tail.Next = item;
+                tail = item;
+            }
+        }
+
+        public int Dequeue()
+        {
+            if (head == null) throw new InvalidOperationException();
+            var result = head.Value;
+            head = head.Next;
+            if (head == null)
+                tail = null;
+            return result;
+
+        }
+    }
+}
