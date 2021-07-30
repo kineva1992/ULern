1dd35685bb7a7d1a89e0ef3dd15dfac93a497eea
alexandex <kienva1992@gmail.com>
2021-07-30 20:19:06 +0300

    feat: new fucntion Undo - undo the last action in history. And CanUndo - return true if the history action is not empty at the moment. that is if the call "Undo" will be correct else, the method must return false.

--- Leavel 2/LimitedSizeStack/ListModel.cs
+++ Leavel 2/LimitedSizeStack/ListModel.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 
 namespace TodoApplication
 {
+    internal class Action<TItem>
+    {
+        internal ActionType ActionType { get; }
+        internal TItem Item { get; }
+        internal int Index { get; }
+        internal Action(ActionType action, TItem item, int index)
+        {
+            ActionType = action;
+            Item = item;
+            Index = index;
+        }
+    }
+    internal enum ActionType
+    { 
+    AddItem,
+    RemoveItem
+    }
+
+
     public class ListModel<TItem>
     {
         public List<TItem> Items { get; }
         public int Limit;
+        private LimitedSizeStack<Action<TItem>> actionStack; 
 
         public ListModel(int limit)
         {
             Items = new List<TItem>();
             Limit = limit;
+            actionStack = new LimitedSizeStack<Action<TItem>>(limit);
         }
 
         public void AddItem(TItem item)
@@ -26,12 +47,18 @@ namespace TodoApplication
 
         public bool CanUndo()
         {
-            return false;
+            return actionStack.Count > 0;
         }
 
         public void Undo()
         {
-            throw new NotImplementedException();
+            var action = actionStack.Pop();
+            if(action.ActionType == ActionType.AddItem)
+            {
+                Items.RemoveAt(action.Index);
+                return;
+            }
+            Items.Insert(action.Index, action.Item);
         }
     }
 }
