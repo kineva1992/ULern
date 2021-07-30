7ad762ee792179f5c57cac393755e59b7b4d4b4e
alexandex <kienva1992@gmail.com>
2021-07-30 18:39:33 +0300

    1

---
+++ Leavel 2/LimitedSizeStack/LimitedSizeStack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApplication
+{
+    public class LimitedSizeStack<T>
+    {
+        private readonly int amount;
+        private LinkedList<T> newLinkedList = new LinkedList<T>();
+        public LimitedSizeStack(int limit)
+        {
+            amount = limit;
+        }
+
+        public void Push(T item)
+        {
+            newLinkedList.AddLast(item);
+            if (newLinkedList.Count > amount)
+                newLinkedList.RemoveFirst();
+        }
+
+        public T Pop()
+        {
+
+            if (newLinkedList.Count == 0)
+                throw new InvalidCastException();
+            var last = newLinkedList.Last.Value;
+            newLinkedList.RemoveLast();
+            return last;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return newLinkedList.Count();
+            }
+        }
+    }
+}
