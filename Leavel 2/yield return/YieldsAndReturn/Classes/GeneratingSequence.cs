561ae1e5849f372492c4749f606d18d04740434f
alexandex <kienva1992@gmail.com>
2021-07-31 14:02:29 +0300

    1

--- Leavel 2/yield return/YieldsAndReturn/Classes/GeneratingSequence.cs
+++ Leavel 2/yield return/YieldsAndReturn/Classes/GeneratingSequence.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace YieldsAndReturn.Classes
 {
-   public static class GeneratingSequence
+    public class GeneratingSequence
     {
         public static IEnumerable<int> GenerateCycle(int maxValue)
         {
@@ -13,19 +13,24 @@ namespace YieldsAndReturn.Classes
             {
                 yield return currnt % maxValue;
                 currnt++;
-            
+
             }
 
-//            private static IEnumerable<int> ZipSum(IEnumerable<int> first, IEnumerable<int> second)
-//            {
-//                var e1 = first.GetEnumerator();
-//                var e2 = second.GetEnumerator();
-//                while (...)
-//        ...
-//}
+            static IEnumerable<int> ZipSum(IEnumerable<int> first, IEnumerable<int> second)
+            {
+                var e1 = first.GetEnumerator();
+                var e2 = second.GetEnumerator();
+                while (e1.MoveNext())
+                {
+                    e2.MoveNext();
+                    yield return e1.Current + e2.Current;
+                }
+
+
+            }
 
 
 
-    }
+        }
     }
 }
--- Leavel 2/yield return/YieldsAndReturn/Program.cs
+++ Leavel 2/yield return/YieldsAndReturn/Program.cs
@@ -1,17 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 using YieldsAndReturn.Classes;
 
 namespace YieldsAndReturn
 {
     class Program
     {
+        static IEnumerable<int> ZipSum(IEnumerable<int> first, IEnumerable<int> second)
+        {
+            var e1 = first.GetEnumerator();
+            var e2 = second.GetEnumerator();
+            while (e1.MoveNext())
+            {
+                e2.MoveNext();
+                yield return e1.Current + e2.Current;
+            }
+
+
+        }
+
         static void Main(string[] args)
         {
            
-            foreach (var number in GeneratingSequence.GenerateCycle(4))
-            {
-                Console.WriteLine(number);
-            }
+            //foreach (var number in GeneratingSequence.GenerateCycle(4))
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            Console.WriteLine(string.Join(" ", ZipSum(new[] { 1 }, new[] { 0 })));
+            Console.WriteLine(string.Join(" ", ZipSum(new[] { 1, 2 }, new[] { 1, 2 })));
+            Console.WriteLine(string.Join(" ", ZipSum(new int[0], new int[0])));
+            Console.WriteLine(string.Join(" ", ZipSum(new[] { 1, 3, 5 }, new[] { 5, 3, -1 })));
+            
         }
     }
 }
