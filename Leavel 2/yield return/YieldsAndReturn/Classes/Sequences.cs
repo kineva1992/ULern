3e3b5fde03bca5b9a4b9132d71fc4ca9cbc152aa
alexandex <kienva1992@gmail.com>
2021-07-31 13:13:29 +0300

    1

---
+++ Leavel 2/yield return/YieldsAndReturn/Classes/Sequences.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YieldsAndReturn.Classes
+{
+    class Sequences
+    {
+		public static IEnumerable<int> Fibonacci
+		{
+			get
+			{
+				var a = 1;
+				var b = 1;
+				yield return 1;
+				yield return 1;
+				while (true)
+				{
+					var c = a + b;
+					a = b;
+					b = c;
+					yield return c;
+				}
+			}
+		}
+		
+		public static IEnumerable<int> Exponential(int multiplier)
+		{
+			var a = 1;
+
+			while (true)
+			{
+				yield return a;
+				a *= multiplier;
+			}
+		}
+	}
+}
