65db6f101ef64577c675d7b65c1403f168d1246e
alexandex <kienva1992@gmail.com>
2021-07-31 12:22:59 +0300

    New project

---
+++ Leavel 2/yield return/YieldsAndReturn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YieldsAndReturn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
