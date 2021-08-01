db7dbf0b379b4bb8f9b55334c440cd19582831da
alexandex <kienva1992@gmail.com>
2021-08-01 22:27:19 +0300

    123

--- Leavel 2/yield return/yield-return-smooth/ExpSmoothingTask.cs
+++ Leavel 2/yield return/yield-return-smooth/ExpSmoothingTask.cs
@@ -1,36 +1,26 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace yield
 {
-    public static class ExpSmoothingTask
-    {
-        public static IEnumerable<DataPoint> SmoothExponentialy(this IEnumerable<DataPoint> data, double alpha)
-        {
-            bool firstTime = true;
-            var currentItem = data.GetEnumerator();
-            double prevY = 0;
-
-            while (currentItem.MoveNext())
-            {
-                double tempY = currentItem.Current.OriginalY;
-                if (firstTime)
-                {
-                    tempY = GetSmoothedY(tempY, prevY, alpha + 1 - alpha);
-                    firstTime = false;
-                }
-                else
-                {
-                    tempY = GetSmoothedY(tempY, prevY, alpha);
-                }
-
-                currentItem.Current.ExpSmoothedY = prevY = tempY;
-                yield return currentItem.Current;
-            }
-        }
-
-        static double GetSmoothedY(double y, double preY, double alpha)
-        {
-            return alpha * y + (1 - alpha) * preY;
-        }
-    }
+	public static class ExpSmoothingTask
+	{
+		public static IEnumerable<DataPoint> SmoothExponentialy(this IEnumerable<DataPoint> data, double alpha)
+		{
+			bool isFirstElement = true;
+			double lastDataPoint = 0;
+			foreach (DataPoint dataPoint in data)
+			{
+				if (isFirstElement)
+				{
+					lastDataPoint = dataPoint.OriginalY;
+					isFirstElement = false;
+				}
+				else
+					lastDataPoint = alpha * dataPoint.OriginalY + (1 - alpha) * lastDataPoint;
+				var newDataPoint = dataPoint.WithExpSmoothedY(lastDataPoint);
+				yield return newDataPoint;
+			}
+		}
+	}
 }
