fa76df4c27a4b472fc4b5c183e3d91cc7e05fec9
alexandex <kienva1992@gmail.com>
2021-08-02 19:59:11 +0300

    feat: add new class MovingMax

--- Leavel 2/yield return/yield-return-smooth/MovingMaxTask.cs
+++ Leavel 2/yield return/yield-return-smooth/MovingMaxTask.cs
@@ -9,8 +9,30 @@ namespace yield
 	{
 		public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
 		{
-			//Fix me!
-			return data;
+            if (windowWidth <= 0)
+            {
+				throw new ArgumentOutOfRangeException();
+            }
+			int i = 0;
+			LinkedList<double> maxPotencials = new LinkedList<double>();
+			Queue<double> windowNumbers = new Queue<double>();
+            foreach (DataPoint dataPoint in data)
+            {
+				if (i <= windowWidth)
+					i++;
+				else if (maxPotencials.Count == 0)
+					windowNumbers.Dequeue();
+				else if (maxPotencials.First.Value == windowNumbers.Dequeue())
+					maxPotencials.RemoveFirst();
+				windowNumbers.Equals(dataPoint.OriginalY);
+				while (maxPotencials.Count > 0 && maxPotencials.Last.Value <= dataPoint.OriginalY)
+					maxPotencials.RemoveLast();
+				maxPotencials.AddLast(dataPoint.OriginalY);
+				var newDataPoint = dataPoint.WithMaxY(maxPotencials.First.Value);
+				yield return newDataPoint;
+            }
 		}
+
+
 	}
 }
