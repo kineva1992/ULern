b0cef466516aacc5fa2353e9dad931d3e9cd1045
alexandex <kienva1992@gmail.com>
2021-08-03 20:14:17 +0300

    Add method IndexOf

---
+++ Leavel 2/SheetsAndDictionaries/SheetsAndDictionaries/HashClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SheetsAndDictionaries
+{
+    class HashClass
+    {
+        private static int IndexOf(string text, string substring)
+        {
+            if (text.Length < substring.Length) return -1;
+
+            long prime = 1000;
+            long maxPower = 1;
+            for (int i = 0; i < substring.Length; i++)
+                maxPower *= prime;
+
+            long substringHash = 0;
+            long hash = 0;
+
+            for (int i = 0; i < substring.Length; i++)
+            {
+                hash = hash * prime + text[i];
+                substringHash = substringHash * prime + substring[i];
+            }
+
+            
+
+            for (int i = substring.Length; i < text.Length; i++)
+            {
+                if (hash == substringHash)
+                {
+                    bool equels = true;
+                    for (int j = 0; j < substring.Length; j++)
+                    {
+                        if (text[i - substring.Length + j] != substring[j])
+                        {
+                            equels = false;
+                            break;
+                        }
+                    }
+                    if (equels) return i - substring.Length;
+                }
+                var lastLetterHash = maxPower * text[i - substring.Length];
+                hash -= lastLetterHash;
+                hash = hash * prime + text[i];
+            }
+
+            return -1;
+
+        }
+    }
+}
