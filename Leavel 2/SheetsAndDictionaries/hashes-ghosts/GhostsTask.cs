5408b3d543da46b7ecdf711db23c5f61d52c9d94
alexandex <kienva1992@gmail.com>
2021-08-04 22:23:09 +0300

    123

---
+++ Leavel 2/SheetsAndDictionaries/hashes-ghosts/GhostsTask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+
+namespace hashes
+{
+	public class GhostsTask : 
+		IFactory<Document>, IFactory<Vector>, IFactory<Segment>, IFactory<Cat>, IFactory<Robot>, 
+		IMagic
+	{
+		
+	}
+}
