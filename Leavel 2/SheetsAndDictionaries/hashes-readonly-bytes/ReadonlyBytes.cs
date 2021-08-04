7c95564d9af9c50f174f60d83d019ca386679f76
alexandex <kienva1992@gmail.com>
2021-08-04 22:04:54 +0300

    123

---
+++ Leavel 2/SheetsAndDictionaries/hashes-readonly-bytes/ReadonlyBytes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace hashes
+{
+    // TODO: Создайте класс ReadonlyBytes
+    public class ReadonlyBytes : IEnumerable<byte>
+    {
+        public int Length => _list.Length;
+        private readonly byte[] _list;
+        private int _hash;
+
+        public ReadonlyBytes(params byte[] array)
+        {
+            _list = array ?? throw new ArgumentNullException();
+
+        }
+
+        private void HashCodeInside()
+        {
+            const int fnvPrime = 3123423;
+            foreach (var item in _list)
+            {
+                unchecked
+                { 
+                }
+            }
+        }
+
+        public IEnumerator<byte> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
