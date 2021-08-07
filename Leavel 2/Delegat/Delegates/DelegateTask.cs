2ead908c0bb7973e4c0080082cafb00fea56ba83
alexandex <kienva1992@gmail.com>
2021-08-07 12:31:14 +0300

    123

---
+++ Leavel 2/Delegat/Delegates/DelegateTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates
+{
+    class DelegateTask
+    {
+		delegate bool TryGet<T1, T2>(string questionText, Action<string> tellUser, out int age);
+		static void Main()
+		{
+			Run(AskUser, Console.WriteLine);
+		}
+
+		static void Run(TryGet<string,int> askUser, Action<string> tellUser)
+		{
+			int age;
+			if (askUser("What is your age?", tellUser, out age))
+				tellUser("Age: " + age);
+		}
+
+		static bool AskUser(string questionText, Action<string> tellUser, out int age)
+		{
+			tellUser(questionText);
+			var answer = Console.ReadLine();
+			return int.TryParse(answer, out age);
+		}
+	}
+}
