f3f411a5607385cbe0ebd349b4dc6ea894210845
alexandex <kienva1992@gmail.com>
2021-08-09 17:37:50 +0300

    123

--- Leavel 2/Delegat/func-brainfuck/VirtualMachine.cs
+++ Leavel 2/Delegat/func-brainfuck/VirtualMachine.cs
@@ -9,19 +9,28 @@ namespace func.brainfuck
 		public int InstructionPointer { get; set; }
 		public byte[] Memory { get; }
 		public int MemoryPointer { get; set; }
+		public Dictionary<char, Action<IVirtualMachine>> RegCommand;// = new Dictionary<char, Action<IVirtualMachine>>();
 
 		public VirtualMachine(string program, int memorySize)
 		{
+			Instructions = program;
+			Memory = new byte[memorySize];
+			InstructionPointer = 0;
+			MemoryPointer = 0;
+			RegCommand = new Dictionary<char, Action<IVirtualMachine>>();
 		}
 
 		public void RegisterCommand(char symbol, Action<IVirtualMachine> execute)
 		{
-			throw new NotImplementedException();
+			RegCommand.Add(symbol, execute);
 		}
 
 		public void Run()
 		{
-			throw new NotImplementedException();
+			for (int i = InstructionPointer; InstructionPointer < Instructions.Length; InstructionPointer++)
+			{
+				if (RegCommand.ContainsKey(Instructions[InstructionPointer])) RegCommand[Instructions[InstructionPointer]].Invoke(this);
+			}
 		}
 	}
 }
