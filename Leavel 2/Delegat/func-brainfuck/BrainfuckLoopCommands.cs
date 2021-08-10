206f6768c78121033de510a4d058d03afe9983f7
alexandex <kienva1992@gmail.com>
2021-08-10 19:40:11 +0300

    Add class BrainfuckLoopCommands

--- Leavel 2/Delegat/func-brainfuck/BrainfuckLoopCommands.cs
+++ Leavel 2/Delegat/func-brainfuck/BrainfuckLoopCommands.cs
@@ -6,8 +6,37 @@ namespace func.brainfuck
 	{
 		public static void RegisterTo(IVirtualMachine vm)
 		{
-			vm.RegisterCommand('[', b => { });
-			vm.RegisterCommand(']', b => { });
+			Dictionary<int, int> indexes = new Dictionary<int, int>();
+			FindBracketsIndexes(vm.Instructions, indexes);
+
+			vm.RegisterCommand('[', b => {
+				if (b.Memory[b.MemoryPointer] == 0)
+					b.InstructionPointer = indexes[b.InstructionPointer];
+			});
+
+
+			vm.RegisterCommand(']', b => {
+				if (b.Memory[b.MemoryPointer] != 0)
+					b.InstructionPointer = indexes[b.InstructionPointer];
+
+			});
+		}
+
+		private static void FindBracketsIndexes(string command, Dictionary<int,int> indexes)
+		{
+			var bracketIndexs = new Stack<int>();
+            for (int i = 0; i < command.Length; i++)
+            {
+				if (command[i] == '[')
+					bracketIndexs.Push(i);
+				else if (command[i] == ']')
+				{
+					var t = bracketIndexs.Pop();
+					indexes.Add(t, i);
+					indexes.Add(i, t);
+				}
+
+            }
 		}
 	}
 }
