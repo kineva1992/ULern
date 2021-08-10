ef7497aeb73fd055bd3136406c5f89d906eabe79
alexandex <kienva1992@gmail.com>
2021-08-10 19:30:59 +0300

    123

--- Leavel 2/Delegat/func-brainfuck/BrainfuckBasicCommands.cs
+++ Leavel 2/Delegat/func-brainfuck/BrainfuckBasicCommands.cs
@@ -8,10 +8,19 @@ namespace func.brainfuck
 	{
 		public static void RegisterTo(IVirtualMachine vm, Func<int> read, Action<char> write)
 		{
-			vm.RegisterCommand('.', b => { });
-			vm.RegisterCommand('+', b => {});
-			vm.RegisterCommand('-', b => {});
-			//...
+			vm.RegisterCommand('.', b => write((char)b.Memory[b.MemoryPointer]));
+			unchecked
+			{
+				vm.RegisterCommand('+', b => b.Memory[b.MemoryPointer]++);
+				vm.RegisterCommand('-', b => b.Memory[b.MemoryPointer]--);
+			}
+			foreach (char i in Enumerable.Range('0', 10).Concat(Enumerable.Range('A', 26).Concat(Enumerable.Range('a', 26))))
+				vm.RegisterCommand(i, b => b.Memory[b.MemoryPointer] = (byte)i);
+
+			vm.RegisterCommand(',', b => b.Memory[b.MemoryPointer] = (byte)read());
+			vm.RegisterCommand('>', b => { b.MemoryPointer = ++b.MemoryPointer % b.Memory.Length; });
+			vm.RegisterCommand('<',
+				b => { b.MemoryPointer = --b.MemoryPointer < 0 ? b.Memory.Length - 1 : b.MemoryPointer; });
 		}
 	}
 }
