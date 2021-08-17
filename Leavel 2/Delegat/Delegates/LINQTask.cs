18a3a93c0fe2ce7159263fd87af31a64954a3c09
alexandex <kienva1992@gmail.com>
2021-08-17 18:09:20 +0300

    q

---
+++ Leavel 2/Delegat/Delegates/LINQTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Delegates
+{
+    class LINQTask
+    {
+
+        private static T FirstOrDefault<T>(IEnumerable<T> source, Func<T, bool> filter)
+        {
+            foreach (var element in source)
+            {
+                if (filter(element))
+                {
+                    return element;
+                }
+            }
+            return default;
+        }
+
+        private static T FirstOrDefaultVer2<T>(IEnumerable<T> soccer, Func<T, bool> filter)
+        {
+            return soccer.FirstOrDefault(e => filter(e));
+        }
+    }
+
+    // TakeTask 
+}
