b54f1dc870130d04a9257b1c9460014d18e16adc
alexandex <kienva1992@gmail.com>
2021-08-17 19:01:41 +0300

    q

--- Leavel 2/Delegat/func-rocket/ControlTask.cs
+++ Leavel 2/Delegat/func-rocket/ControlTask.cs
@@ -6,7 +6,17 @@ namespace func_rocket
 	{
 		public static Turn ControlRocket(Rocket rocket, Vector target)
 		{
-			return Turn.None;
+			var distanceVector = target - rocket.Location;
+			double totalAngle = distanceVector.Angle - rocket.Direction;
+			if (Math.Abs(distanceVector.Angle - rocket.Direction) < 0.5
+				|| Math.Abs(distanceVector.Angle - rocket.Velocity.Angle) < 0.5)
+				totalAngle = (totalAngle + distanceVector.Angle - rocket.Velocity.Angle) / 2;
+			if (totalAngle > 0)
+				return Turn.Right;
+			if (totalAngle < 0)
+				return Turn.Left;
+			else
+				return Turn.None;
 		}
 	}
 }
