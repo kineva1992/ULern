1ba9fde72ea2284bb33ec8d323a7aac6cb65e621
alexandex <kienva1992@gmail.com>
2021-08-21 22:04:49 +0300

    123

---
+++ Leavel 1/FirstStep/FirstSteps/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FirstSteps
+{
+    class Program
+    {
+        private static string GetGreetingMessage(string name, double salary)
+        {
+            return $"Hello, {name}, your salary is {salary}";
+        }
+
+        private static void Print(double getSquere)
+        {
+            Console.WriteLine(getSquere);
+        }
+        private static double GetSquare(double sqrt)
+        {
+            return Math.Pow(sqrt, 2);
+        }
+
+        private static string GetLastHalf(string text)
+        {
+            return text.Substring(text.Length / 2).Replace(" ", null);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetGreetingMessage("Student", 10.01));
+            Console.WriteLine(GetGreetingMessage("Bill Gates", 10000000.5));
+            Console.WriteLine(GetGreetingMessage("Steve Jobs", 1));
+
+            Print(GetSquare(42));
+
+            Console.WriteLine(GetLastHalf("I love CSharp!"));
+            Console.WriteLine(GetLastHalf("1234567890"));
+            Console.WriteLine(GetLastHalf("до ре ми фа соль ля си"));
+        }
+    }
+}
