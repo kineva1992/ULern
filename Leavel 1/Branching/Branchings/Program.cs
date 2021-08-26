a0082d46a2642b961a55dbde2146de81dc8d9f3b
alexandex <kienva1992@gmail.com>
2021-08-26 20:46:54 +0300

    123

---
+++ Leavel 1/Branching/Branchings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Branchings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.TestMove("a1", "d4");
+            Task.TestMove("f4", "e7");
+            Task.TestMove("a1", "a4");
+        }
+    }
+}
---
+++ Leavel 1/Branching/Branchings/Task.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Branchings
+{
+   public static class Task
+    {
+        private static bool IsCorrectMove(string from, string to)
+        {
+            var dx = Math.Abs(to[0] - from[0]); //смещение фигуры по горизонтали
+            var dy = Math.Abs(to[1] - from[1]); //смещение фигуры по вертикали
+
+            if (dx == 0 && dy == 0)
+            {
+                return false;
+            }
+            else if (dx == 0 || dy == 0)
+            {
+                return true;
+            }
+            else if (dx == dy)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        
+
+        public static void TestMove(string from, string to)
+        {
+            Console.WriteLine("{0} - {1} {2}", from, to, IsCorrectMove(from,to));
+        }
+
+    }
+}
