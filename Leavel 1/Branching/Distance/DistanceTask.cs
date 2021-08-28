d4109fd762459414cbd7a85239563a73172ead52
alexandex <kienva1992@gmail.com>
2021-08-28 20:39:58 +0300

    123

--- Leavel 1/Branching/Distance/DistanceTask.cs
+++ Leavel 1/Branching/Distance/DistanceTask.cs
@@ -12,7 +12,16 @@ namespace DistanceTask
 		// Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)
 		public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
 		{
-			return 0.0;
+			var sx = bx - ax;
+			var sy = by - ay;
+
+			var squarePA = Sqr(x - ax) + Sqr(y - ay);
+			var squarePB = Sqr(x - bx) + Sqr(y - by);
+			var squareAB = Sqr(sx) + Sqr(sy);
+
+			return (squareAB > Math.Abs(squarePA - squarePB)
+				? Math.Abs(sy * x - sx * y + bx * ay - by * ax) / Math.Sqrt(squareAB)
+				: Math.Sqrt(Math.Min(squarePA, squarePB)));
 		}
 	}
 }
