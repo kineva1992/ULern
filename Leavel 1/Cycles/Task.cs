9c9bfb984d01da298cecb18e178d32a7a0af2260
alexandex <kienva1992@gmail.com>
2021-08-30 20:52:57 +0300

    End one task

--- Leavel 1/Cycles/Task.cs
+++ Leavel 1/Cycles/Task.cs
@@ -6,5 +6,24 @@ namespace Cycles
 {
     class Task
     {
+        public static int GetMinPowerOfTwoLargerThan(int number)
+        {
+            if (number < 0) throw new ArgumentNullException();
+
+            else { 
+            int result = 1;
+            while (Math.Pow(2, result) <= number)
+            {
+                result++;
+            }
+
+            result = (int)Math.Pow(2,result);
+                return result;    
+            }
+
+            }
+            
+        }
+
     }
-}
+
