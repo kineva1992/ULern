57d4a8774eafce39364c450b0fda46f63465e618
kineva1992 <42785264+kineva1992@users.noreply.github.com>
2021-08-31 11:07:32 +0300

    Update Task.cs

--- Leavel 1/Cycles/Task.cs
+++ Leavel 1/Cycles/Task.cs
@@ -6,9 +6,16 @@ namespace Cycles
 {
     static class Task
     {
+    /*
+    Найдите минимальную степень двойки, превосходящую заданное число.
+
+    Более формально: для заданного числа nn найдите x > nx>n, такой, что x = 2^kx=2 
+    k для некоторого натурального kk.
+
+    Решите эту задачу с помощью цикла while.
+    */
         public static int GetMinPowerOfTwoLargerThan(int number)
         {
-            
             int result = 1;
             while (number >= result)
             {
@@ -16,8 +23,18 @@ namespace Cycles
             }
 
             return result;
-            
+        }
+        
+        /*
+        Враги вставили в начало каждого полезного текста целую кучу бесполезных пробельных символов!
 
+        Вася смог справиться с ситуацией, когда такой пробел был один, но продвинуться дальше ему не удается. Помогите ему с помощью цикла while.
+        */
+        public static string RemoveStartSpaces(sting inputText)
+        {
+        
+        
+        
         }
 
     }
