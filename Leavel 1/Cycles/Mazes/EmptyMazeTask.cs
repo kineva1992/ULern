a039ece6f0df270b8e53e7ed75e5ac1c94e18f71
alexandex <kienva1992@gmail.com>
2021-09-05 19:10:00 +0300

    123

---
+++ Leavel 1/Cycles/Mazes/EmptyMazeTask.cs
@@ -0,0 +1,24 @@
+﻿namespace Mazes
+{
+	public static class EmptyMazeTask
+	{
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+		}
+
+		private static void MoveRight(Robot robot, int stepCount)
+		{
+            for (int i = 0; i < stepCount - 3; i++)
+            {
+				robot.MoveTo(Direction.Right);
+            }
+		}
+		private static void MoveLeft(Robot robot, int stepCount)
+		{
+			for (int i = 0; i < stepCount - 3; i++)
+			{
+				robot.MoveTo(Direction.Left);
+			}
+		}
+	}
+}
