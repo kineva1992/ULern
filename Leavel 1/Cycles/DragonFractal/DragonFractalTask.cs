25ce7931d4736e053e5b35496cb553cf09afeb82
alexandex <kienva1992@gmail.com>
2021-09-19 00:59:58 +0300

    123

--- Leavel 1/Cycles/DragonFractal/DragonFractalTask.cs
+++ Leavel 1/Cycles/DragonFractal/DragonFractalTask.cs
@@ -1,32 +1,48 @@
 ﻿// В этом пространстве имен содержатся средства для работы с изображениями. 
 // Чтобы оно стало доступно, в проект был подключен Reference на сборку System.Drawing.dll
+using System;
 using System.Drawing;
 
 namespace Fractals
 {
 	internal static class DragonFractalTask
 	{
-		public static void DrawDragonFractal(Pixels pixels, int iterationsCount, int seed)
+		private static double GetXCordinat(double x, double y, double angle)
 		{
-			/*
-			Начните с точки (1, 0)
-			Создайте генератор рандомных чисел с сидом seed
-			
-			На каждой итерации:
+			return (x * Math.Cos(angle) - y * Math.Cos(angle) / Math.Sqrt(2));
+		}
 
-			1. Выберите случайно одно из следующих преобразований и примените его к текущей точке:
+		private static double GetYCordinat(double x, double y, double angle)
+		{
+			return (x * Math.Cos(angle) + y * Math.Cos(angle) / Math.Sqrt(2));
+		}
 
-				Преобразование 1. (поворот на 45° и сжатие в sqrt(2) раз):
-				x' = (x · cos(45°) - y · sin(45°)) / sqrt(2)
-				y' = (x · sin(45°) + y · cos(45°)) / sqrt(2)
+		internal static void DrawDragonFractal(Pixels pixels, int iterationsCount, int seed)
+		{
+			double x = 1.0;
+			double y = 0.0;
+			const double PI = Math.PI;
 
-				Преобразование 2. (поворот на 135°, сжатие в sqrt(2) раз, сдвиг по X на единицу):
-				x' = (x · cos(135°) - y · sin(135°)) / sqrt(2) + 1
-				y' = (x · sin(135°) + y · cos(135°)) / sqrt(2)
-		
-			2. Нарисуйте текущую точку методом pixels.SetPixel(x, y)
+			Random randomNumberInt = new Random();
 
-			*/
+			for (int i = 0; i < iterationsCount; i++)
+			{
+				var oldX = x;
+				if (randomNumberInt.Next(2) == 0)
+				{
+					x = GetXCordinat(x, y, Math.PI / 4);
+					y = GetYCordinat(x, y, Math.PI / 4);
+				}
+
+				else
+				{
+					x = GetXCordinat(x, y, PI * 3 / 4) + 1;
+					y = GetYCordinat(oldX, y, PI * 3 / 4);
+				}
+
+				pixels.SetPixel(x,y);
+			}
+
 		}
 	}
 }
