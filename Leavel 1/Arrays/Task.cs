78d06fe0409f39c0280d368faaad3310dce57c65
alexandex <kienva1992@gmail.com>
2021-09-19 16:09:49 +0300

    123

---
+++ Leavel 1/Arrays/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    class Task
+    {
+        public static int[] GetFirstEvenNumbers(int count)
+        {
+            int[] resultNums = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                resultNums[i] = (i + 1) * 2;
+            }
+
+            return resultNums;
+        }
+    }
+}
