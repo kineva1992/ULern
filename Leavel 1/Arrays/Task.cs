e622788be9aef0722ab3b90336856c8163d74d9d
alexandex <kienva1992@gmail.com>
2021-09-19 16:24:29 +0300

    123

--- Leavel 1/Arrays/Task.cs
+++ Leavel 1/Arrays/Task.cs
@@ -16,5 +16,45 @@ namespace Arrays
 
             return resultNums;
         }
+        /*
+        Чтобы освоиться с массивами, вы с Васей решили потренироваться на простых алгоритмах. Вася написал метод поиска минимума в массиве:
+        А вам выпала задача посложнее — написать метод поиска индекса максимального элемента. 
+        То есть такого числа i, что array[i] — это максимальное из чисел в массиве.
+        Если в массиве максимальный элемент встречается несколько раз, вывести нужно минимальный индекс.
+        Если массив пуст, вывести нужно -1.
+         */
+
+        public static double MaxInArray(double[] array)
+        {
+            var max = double.MinValue;
+            int index = 0;
+
+            if (array.Length == 0) return -1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+
+        public static int GetElementCount(int[] items, int itemToCount)
+        {
+            int CountInArray = 0;
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] == itemToCount)
+                {
+                    CountInArray++;
+                }
+            }
+
+            return CountInArray;
+        }
     }
 }
