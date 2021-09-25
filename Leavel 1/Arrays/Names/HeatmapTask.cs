c68c1351f5492000d46c61aa94b48158bb5dda69
alexandex <kienva1992@gmail.com>
2021-09-25 16:11:14 +0300

    123

--- Leavel 1/Arrays/Names/HeatmapTask.cs
+++ Leavel 1/Arrays/Names/HeatmapTask.cs
@@ -6,11 +6,33 @@ namespace Names
     {
         public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
         {
-            return new HeatmapData(
-                "Пример карты интенсивностей",
-                new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, 
-                new[] { "a", "b", "c", "d" }, 
-                new[] { "X", "Y", "Z" });
+            var days = new string[30];
+            var mounts = new string[12];
+            var heat = new double[30, 12];
+
+            //Приведение дней в строку.
+            for (int i = 2; i < days.Length + 2; i++)
+            {
+                days[i - 2] = i.ToString();
+            }
+
+            //Приведение месяцев в строку.
+            for (int i = 1; i < mounts.Length + 1; i++)
+            {
+                mounts[i - 1] = i.ToString();
+            }
+            //
+            foreach (var item in names)
+            {
+                if (item.BirthDate.Day != 1)
+                {
+                    heat[item.BirthDate.Day - 2, item.BirthDate.Month - 1] += 1;
+                }
+            }
+            return new HeatmapData("Пример карты интенсивностей", 
+                heat, 
+                days, 
+                mounts);
         }
     }
 }
--- Leavel 1/Arrays/Names/HistogramTask.cs
+++ Leavel 1/Arrays/Names/HistogramTask.cs
@@ -7,10 +7,27 @@ namespace Names
     {
         public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
         {
+            
+
+            double[] daysCount = new double[31];
+            string[] daysOfMounth = new string[31];
+
+            for (int i = 1; i <= 31; i++)
+            {
+                daysOfMounth[i - 1] = i.ToString();
+            }
+
+            foreach (var item in names)
+            {
+                if (item.Name == name && item.BirthDate.Day != 1)
+                {
+                    daysCount[item.BirthDate.Day - 1] += 1;
+                }
+            }
             return new HistogramData(
-                string.Format("Рождаемость людей с именем '{0}'", name), 
-                new string[0], 
-                new double[0]);
+                string.Format("Рождаемость людей с именем '{0}'", name),
+                daysOfMounth,
+                daysCount);
         }
     }
 }
