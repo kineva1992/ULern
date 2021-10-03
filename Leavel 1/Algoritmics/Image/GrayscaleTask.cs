4dc1a0ff0e783c9823ef1993c67aa38508999fc8
alexandex <kienva1992@gmail.com>
2021-10-03 17:07:32 +0300

    123

--- Leavel 1/Algoritmics/Image/GrayscaleTask.cs
+++ Leavel 1/Algoritmics/Image/GrayscaleTask.cs
@@ -20,7 +20,21 @@
 
 		public static double[,] ToGrayscale(Pixel[,] original)
 		{
-			return new double[original.GetLength(0), original.GetLength(1)];
+			var lenX = original.GetLength(0);
+			var lenY = original.GetLength(1);
+			var grayScale = new double[lenX, lenY];
+
+			for (int i = 0; i < lenX; i++)
+			{
+				for (int j = 0; j < lenY; j++)
+				{
+					var red = original[i, j].R;
+					var green = original[i, j].G;
+					var blue = original[i, j].B;
+					grayScale[i, j] = ((0.299 * red) + (0.587 * green) + (0.114 * blue)) / 255;
+				}
+			}
+			return grayScale;
 		}
 	}
 }
