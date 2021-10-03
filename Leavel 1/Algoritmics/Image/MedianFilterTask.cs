0db2cafda4450931144f89f68e4affec727e3e31
alexandex <kienva1992@gmail.com>
2021-10-03 17:26:38 +0300

    123

--- Leavel 1/Algoritmics/Image/MedianFilterTask.cs
+++ Leavel 1/Algoritmics/Image/MedianFilterTask.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Recognizer
 {
@@ -15,7 +16,33 @@ namespace Recognizer
 		 */
 		public static double[,] MedianFilter(double[,] original)
 		{
-			return original;
+			var lenX = original.GetLength(0);
+			var lenY = original.GetLength(1);
+			var currentMedian = new List<double>();
+			var newImages = new double[lenX, lenY];
+
+			for (int i = 0; i < lenX; i++)
+			{
+				for (int j = 0; j < lenY; j++)
+				{
+					for (int ii = -1; ii < 2; ii++)
+						for (int jj = -1; jj < 2; jj++)
+							if (i + ii >= 0 && j + jj >= 0 && i + ii < lenX && j + jj < lenY)
+								currentMedian.Add(original[i + ii, j + jj]);
+
+					currentMedian.Sort();
+					var count = currentMedian.Count();
+
+					if (count % 2 == 0)
+						newImages[i, j] = (currentMedian[count / 2] + currentMedian[(count / 2 - 1)]) / 2;
+
+					else
+						newImages[i, j] = currentMedian[count / 2];
+
+				}
+			}
+
+			return newImages;
 		}
 	}
 }
--- Leavel 1/Algoritmics/Image/ThresholdFilterTask.cs
+++ Leavel 1/Algoritmics/Image/ThresholdFilterTask.cs
@@ -1,10 +1,49 @@
-﻿namespace Recognizer
+﻿using System.Collections.Generic;
+
+namespace Recognizer
 {
 	public static class ThresholdFilterTask
 	{
 		public static double[,] ThresholdFilter(double[,] original, double whitePixelsFraction)
 		{
-			return new double[original.GetLength(0), original.GetLength(1)];
+			var lenX = original.GetLength(0);
+			var lenY = original.GetLength(1);
+			var allPixels = new List<double>();
+			var t = 0.0;
+			double whitePixelCount;
+
+			for (int x = 0; x <= 1; x++)
+			{
+				for (int i = 0; i < lenX; i++)
+				{
+					for (int j = 0; j < lenY; j++)
+					{
+						if (x == 0)
+							allPixels.Add(original[i, j]);
+						else
+						{
+							if (original[i, j] >= t)
+								original[i, j] = 1.0;
+							else
+								original[i, j] = 0.0;
+						}
+					}
+
+				}
+				allPixels.Sort();
+				var count = allPixels.Count;
+				var orignalLength = original.Length;
+				whitePixelCount = (orignalLength * whitePixelsFraction);
+				if (whitePixelCount == 0.0)
+					t = int.MaxValue;
+				else
+				{
+					var tIndex = (int)(count - whitePixelCount);
+					t = allPixels[tIndex];
+				}
+			}
+			return original;
+
 		}
 	}
 }
