20d2ac031acb7740bca1173eca4b54674610f8fb
alexandex <kienva1992@gmail.com>
2021-10-03 17:01:02 +0300

    123

---
+++ Leavel 1/Algoritmics/Algoritm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algoritm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
